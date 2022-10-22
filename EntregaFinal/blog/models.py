1d7c635715bad31e3bfa45e6264720e258ae6183
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2022-10-22 23:09:45 +0200

    Se crean los modelos del proyecto con sus atributos, se agrega la app al settings, se realizan la migraciones, se prueba en la db

--- EntregaFinal/blog/models.py
+++ EntregaFinal/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Models
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    nombre_usuario = models.CharField(max_length=40)
+    contraseña = models.CharField(max_length=40)
+
+
+class Pagina(models.Model):
+    titulo = models.CharField(max_length=40)
+    subtitulo = models.CharField(max_length=40)
+    cuerpo = models.CharField(max_length=256)
+    autor = models.CharField(max_length=40)
+    fecha = models.DateField()
+    imagen = models.FileField()
