f62a8deaa29a758217707da74684c1e791acc228
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2022-10-23 22:33:46 +0200

    Se crea el template de inicio y el de navegacion que eredaran las paginas de la app, se agrega el template bootstrap para el nav de la app

--- EntregaFinal/blog/urls.py
+++ EntregaFinal/blog/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from blog.views import mostrar_inicio
 
 urlpatterns = [
-    path('inicio/', mostrar_inicio),
+    path("", mostrar_inicio, name='Inicio'),
 ]
