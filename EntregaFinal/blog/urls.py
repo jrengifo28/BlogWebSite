cd5d73df0c9d939b70464fbd7ebe5c5578ed3fa9
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2022-10-26 14:42:36 +0200

    Se modifican los archivos de views y urls

--- EntregaFinal/blog/urls.py
+++ EntregaFinal/blog/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import mostrar_inicio
+from blog.views import mostrar_inicio,  UsuarioList
+""" listar_usuarios,"""
 
 urlpatterns = [
     path("", mostrar_inicio, name='Inicio'),
+    # path("lista-usuarios", listar_usuarios),
+    path("usuario/list", UsuarioList.as_view(), name="UsuarioList"),
 ]
--- EntregaFinal/blog/views.py
+++ EntregaFinal/blog/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from blog.models import Usuario, Pagina
+
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 # Create your views here.
 
 
 def mostrar_inicio(request):
     return render(request, "blog/inicio.html")
+
+
+class UsuarioList(ListView):
+    model = Usuario
+    template_name = "blog/listar-usuarios.html"
+
+# def listar_usuarios(request):
+#     all_users = Usuario.objects.All()
+#     contexto = {"usuarios_encontrados": all_users}
+#     return render(request, "blog/listar-usuarios.html", contexto)
+
+
+class PaginaList(ListView):
+    model = Pagina
+    template_name = "blog/pagina-list.html"
