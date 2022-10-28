6fc0e8e4f0efec1a1298a4378f79233187ccb2b9
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2022-10-28 11:45:52 +0200

    Se modificaron los html de las páginas de navegación para que funcionara el boton de login, se agregó al settings la url default, se realizaron ajustes de visualización en los html de listar usuario, listar páginas y usuario detalle

--- EntregaFinal/EntregaFinal/settings.py
+++ EntregaFinal/EntregaFinal/settings.py
@@ -125,3 +125,5 @@ STATICFILE_DIRS = (os.path.join(BASE_DIR, "EntregaFianl/static"),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/blog'
