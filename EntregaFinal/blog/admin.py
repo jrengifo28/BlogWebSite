caf3dc825fcbf1ccd460849ad80444aedbcb1051
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2022-10-31 13:46:44 +0100

    Se eliminó el modelo de usuario, sólo se dejó el modelo de páginas

--- EntregaFinal/blog/admin.py
+++ EntregaFinal/blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from blog.models import Usuario, Pagina
+from blog.models import Pagina
 
 # Register your models here.
-admin.site.register(Usuario)
 admin.site.register(Pagina)
--- EntregaFinal/blog/models.py
+++ EntregaFinal/blog/models.py
@@ -3,17 +3,6 @@ from django.db import models
 # Models
 
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=40)
-    apellido = models.CharField(max_length=40)
-    email = models.EmailField()
-    nombre_usuario = models.CharField(max_length=40)
-    contraseña = models.CharField(max_length=40)
-
-    def __str__(self):
-        return f"({self.nombre_usuario}) {self.nombre} {self.apellido}"
-
-
 class Pagina(models.Model):
     titulo = models.CharField(max_length=40)
     subtitulo = models.CharField(max_length=40)
